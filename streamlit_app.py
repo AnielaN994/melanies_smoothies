f9b0cd799c809594d656600814beb68561e89094
AnielaN994 <aniela-alexandra.nastase@endava.com>
2024-06-25 21:14:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,8 @@ if ingredients_list:
 	    
 	    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 	    
-    #st.write(fruityvice_response)
+	    #df = pd.json_normalize(fruityvice_response)
+    st.write(fruityvice_response)
 
 
 
