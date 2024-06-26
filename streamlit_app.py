00a599aab05c1f28a6ec29e683a63dd2749622f3
AnielaN994 <aniela-alexandra.nastase@endava.com>
2024-06-26 16:01:16 +0300

    Update streamlit_app.py
    Parse columns - variant 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,14 @@ import requests
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom Smoothie!"""
+    """Choose the fruits you want in your custom Smoothie!
+    """
 )
 
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
+
 
 
 cnx = st.connection("snowflake")
@@ -38,28 +40,39 @@ if len(ingredients_list) > 5:
 
 
 if ingredients_list:
-    ingredients_string = ' '.join(ingredients_list)	
+    ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
+	    ingredients_string += fruit_chosen + ' '
+	    
 	    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
 	    #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 	    
 	    st.subheader(fruit_chosen + ' Nutrition Information')
 	    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-	    data = fruityvice_response.json()
-	    df = pd.json_normalize(data)
 	    
-	    # Select specific columns to display
-	    columns_to_display = ['family', 'order', 'nutritions.calories', 'nutritions.fat', 'nutritions.sugar', 'nutritions.carbohydrates', 'nutritions.protein', ]
-	    filtered_df = df[columns_to_display]
-            # Display the filtered DataFrame in Streamlit
-	    st.dataframe(filtered_df, use_container_width=True)
-        
-	    #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+	    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 	    
 	    #df = pd.json_normalize(fruityvice_response)
-    #st.write(fruityvice_response)
+        #st.write(fruityvice_response)
+        # use the color selected to go back and get all the info from the database
+        
+        #table_prod_data = session.sql("select file_name, price, size_list, upsell_product_desc, file_url from catalog_for_website where color_or_style = '" + option + "';")
+        #pd_df = table_prod_data.to_pandas() 
 
+        # assign each column of the row returned to its own variable 
+        family = pd_df['family'].iloc[0])+'0'
+        order = pd_df['order'].iloc[0]
+        calories = pd_df['nutritions.calories'].iloc[0]
+        fat = pd_df['nutritions.fat'].iloc[0]
+        sugar = pd_df['nutritions.sugar'].iloc[0]
+        carbohydrates = pd_df['nutritions.carbohydrates'].iloc[0]
+        protein = pd_df['nutritions.protein'].iloc[0]
+
+        # display the info on the page
+        st.markdown('**Family:** '+ family)
+        st.markdown('**Order:** ' + order)
+        st.markdown('**Calories:** ' + nutritions.calories)
 
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
